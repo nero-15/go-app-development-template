e0bd708681c363eb63861c5bf4b01108d2f3e7be
nero-15 <ti7805@gmail.com>
2021-04-21 23:32:10 +0900

    setting ini

--- struttura/go.mod
+++ struttura/go.mod
@@ -2,4 +2,7 @@ module struttura
 
 go 1.14
 
-require github.com/labstack/echo/v4 v4.2.2
+require (
+	github.com/labstack/echo/v4 v4.2.2
+	gopkg.in/ini.v1 v1.62.0
+)
--- struttura/main.go
+++ struttura/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"net/http"
+	"struttura/config"
 
 	echo "github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(config.Config.Port))
 }
